699cda4a9a083902d2f42167dede3064964ead2c
zean <zean.syu@gmail.com>
2021-04-04 23:36:17 +0900

    fix cors

--- bill/main.py
+++ bill/main.py
@@ -3,6 +3,7 @@ import uvicorn
 from fastapi import APIRouter, FastAPI
 
 from app.core.config import settings
+from fastapi.middleware.cors import CORSMiddleware
 from app.api.endpoints import health, bill
 
 
@@ -16,6 +17,16 @@ api_router.include_router(
 
 app = FastAPI(docs_url=f'{settings.api_base_path}/docs', openapi_url=f'{settings.api_base_path}/openapi.json')
 app.include_router(api_router)
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
